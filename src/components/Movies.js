f28ffc7720260fc0d8c3de4e0cb3557df0f6eb6e
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-06-17 23:55:27 -0700

    complete lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  //const movieList = 
+  
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {
+        movies.map((movie)=>(
+        <div key={movie.title} >
+          <h2>{movie.title}</h2>
+          <h3> {movie.time} </h3>
+          <ul>
+            {movie.genres.map((genre)=>(
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+        ))
+        }
+    </div>
+  )
 }
 
 export default Movies;
